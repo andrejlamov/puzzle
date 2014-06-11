a8cb422b8f2e75d1f4436ddd3b446a383bbec472
Andrej Lamov <andrej.lamov@gmail.com>
2014-06-11 14:14:35 -0400

    Adapt for node

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 var http = require('http'),
     static = require('node-static');
 
-var fileServer = new static.Server(__dirname);
+var fileServer = new static.Server(__dirname + '/www');
 var port = process.argv[2];
 
 http.createServer(function(req, resp) {
   fileServer.serve(req, resp, function(err, res) {
     if(err) {
-      console.log(__dirname,'throwing error',req.url,  err.message);
+      console.log(__dirname, port, 'throwing error', req.url,  err.message);
       resp.writeHead(err.status, err.headers);
       resp.end();
       return;
     }
   });
 }).listen(port, function(){
-  if(process.send) process.send({msg: 'ready'});
+  if(process.send) process.send({msg: 'ready', 'sender': port});
 })
